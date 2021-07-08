d01993aaedc76dbdd3d36644ff342481096b14b7
GhoZaa <ghoza16m@gmail.com>
2021-07-08 17:51:44 +0700

    TODO must do 1: fix bugs

--- specs/likeMovieSpec.js
+++ specs/likeMovieSpec.js
@@ -66,7 +66,7 @@ describe('Liking a Movie', () => {
     FavoriteMovieIdb.deleteMovie(1);
   });
 
-  xit('should not add a moive when it has no id', async () => {
+  it('should not add a moive when it has no id', async () => {
     await LikeButtonInitiator.init({
       likeButtonContainer: document.querySelector('#likeButtonContainer'),
       movie: {},
